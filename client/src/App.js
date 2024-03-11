773034cbda51d8b63dff6cfcc28e12192cd24f26
mirevas <aaaurorastan@gmail.com>
2024-03-10 23:33:50 -0400

    Building homepage with react

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header>
+        <a href="" className="logo">MyBlog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
       </header>
-    </div>
+      <div className="post">
+        <div className="image">
+          <img src="https://randompicturegenerator.com/img/picture-generator/54e2d2424852a514f1dc8460962e33791c3ad6e04e5074417c2e7dd3964cc5_640.jpg" alt="content image"></img>
+        </div>
+        <div className="texts">
+          <h2>Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
+          <p className="info">
+            <a className="author">Aurora Zhang</a>
+            <time>2024-03-10 23:12</time>
+          </p>
+          <p className="summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam vestibulum morbi blandit cursus risus. Cras semper auctor neque vitae tempus quam pellentesque. Ipsum a arcu cursus vitae. Purus semper eget duis at tellus at urna.</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img src="https://randompicturegenerator.com/img/picture-generator/54e2d2424852a514f1dc8460962e33791c3ad6e04e5074417c2e7dd3964cc5_640.jpg" alt="content image"></img>
+        </div>
+        <div className="texts">
+          <h2>Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
+          <p className="info">
+            <a className="author">Aurora Zhang</a>
+            <time>2024-03-10 23:12</time>
+          </p>
+          <p className="summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam vestibulum morbi blandit cursus risus. Cras semper auctor neque vitae tempus quam pellentesque. Ipsum a arcu cursus vitae. Purus semper eget duis at tellus at urna.</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img src="https://randompicturegenerator.com/img/picture-generator/54e2d2424852a514f1dc8460962e33791c3ad6e04e5074417c2e7dd3964cc5_640.jpg" alt="content image"></img>
+        </div>
+        <div className="texts">
+          <h2>Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
+          <p className="info">
+            <a className="author">Aurora Zhang</a>
+            <time>2024-03-10 23:12</time>
+          </p>
+          <p className="summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam vestibulum morbi blandit cursus risus. Cras semper auctor neque vitae tempus quam pellentesque. Ipsum a arcu cursus vitae. Purus semper eget duis at tellus at urna.</p>
+        </div>
+      </div>
+    </main>
   );
 }
 
